af458b36d6d5a7fa9ffd089691db4c1ecdab3dc1
Wataru UEMURA <wataru@rins.ryukoku.ac.jp>
2022-06-01 16:20:38 +0900

    remove the test files.

--- scripts/rosRcllRefBoxLocal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Team: BabyTigers, Color: CYAN, Robot Number: 1
-sleep 2
-pushd ~/catkin_ws/src/ros-rcll_refbox_peer/launch
-roslaunch rcll_refbox_peer.launch num_robots:=1 team_name:=BabyTigers robot_name:=kotora robot_number:=1 crypto_key:=randomkey
-popd
