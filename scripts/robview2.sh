eda1da64dd2f9dc368cd42c28c29c158fc08cd93
Wataru UEMURA <wataru@rins.ryukoku.ac.jp>
2022-06-03 17:30:07 +0900

    change the commands for executing on the remote robotino.

--- scripts/robview2.sh
+++ scripts/robview2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh robotino "~/robview2_interpreter -f ~/catkin_ws/src/ros-rcll_btr_msgs/ros_robotino2.rvw2"
+# ssh robotino "~/robview2_interpreter -f ~/catkin_ws/src/ros-rcll_btr_msgs/ros_robotino2.rvw2"
+ssh robotino "~/robview2_interpreter -f /home/robotino/ros_robotino2.rvw2"
