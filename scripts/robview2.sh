dcd936d581bd45f43b445b67a47ee4020f88d0ef
Wataru UEMURA <wataru@rins.ryukoku.ac.jp>
2022-06-30 17:21:21 +0900

    remove these script file. and move them to team's git directory.

--- scripts/robview2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# ssh robotino "~/robview2_interpreter -f ~/catkin_ws/src/ros-rcll_btr_msgs/ros_robotino2.rvw2"
-ssh robotino "~/robview2_interpreter -f /home/robotino/ros_robotino2.rvw2"
